b3a90e4e0035036bb4af22e4fca409f55e56abfc
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-03-18 12:28:53 -0500

    added rutter link dependency, implemented button to interact with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import * as React from 'react';
+import { useRutterLink, RutterLinkOptions } from 'react-rutter-link';
 
 const App = () => {
-  return <div></div>;
+  const rutterConfig: RutterLinkOptions = {
+    publicKey: process.env.RUTTER_PUBLIC_KEY || '',
+    onSuccess: (publicToken: string) => {
+      console.log(publicToken);
+    },
+  };
+
+  const { open } = useRutterLink(rutterConfig);
+
+  return (
+    <div>
+      <button onClick={() => open()}>Open</button>
+    </div>
+  );
 };
 
 export default App;
