53b2da1150b4dec5870ce3b58ee4de9c8b7d09f0
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-03-18 13:15:46 -0500

    utilized react-router-link to initialize authentication flow and connect to a store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import * as React from 'react';
-import { useRutterLink, RutterLinkOptions } from 'react-rutter-link';
+import axios from 'axios';
+import { useRutterLink } from 'react-rutter-link';
 
 const App = () => {
-  const rutterConfig: RutterLinkOptions = {
-    publicKey: process.env.RUTTER_PUBLIC_KEY || '',
-    onSuccess: (publicToken: string) => {
-      console.log(publicToken);
-    },
+  const exchangeCredentials = async (publicToken: string) => {
+    try {
+      const res = await axios.post(
+        `https://sandbox.rutterapi.com/item/public_token/exchange`,
+        {
+          client_id: process.env.CLIENT_ID,
+          secret: process.env.SECRET_KEY,
+          public_token: publicToken,
+        }
+      );
+      console.log('res.data', res.data);
+    } catch (e: any) {
+      console.log(e);
+    }
   };
 
-  const { open } = useRutterLink(rutterConfig);
+  const { open } = useRutterLink({
+    publicKey: process.env.PUBLIC_KEY || '',
+    onSuccess: (publicToken: string) => {
+      console.log('publicToken', publicToken);
+      exchangeCredentials(publicToken);
+    },
+  });
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
       <button onClick={() => open()}>Open</button>
     </div>
   );
