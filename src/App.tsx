3257c491e74f837db9c118d9210dac099e077ab6
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-03-18 13:30:12 -0500

    added fetchConnection handler to hit [GET] /connections/access_token

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import * as React from 'react';
 import axios from 'axios';
 import { useRutterLink } from 'react-rutter-link';
 
+interface Credentials {
+  access_token: string;
+  connection_id: string;
+  is_ready: boolean;
+  platform: string;
+  request_id: string;
+}
+
 const App = () => {
+  const [credentials, setCredentials] = React.useState<Credentials>();
+
   const exchangeCredentials = async (publicToken: string) => {
     try {
-      const res = await axios.post(
+      const res = await axios.post<Credentials>(
         `https://sandbox.rutterapi.com/item/public_token/exchange`,
         {
           client_id: process.env.REACT_APP_CLIENT_ID,
@@ -13,7 +23,31 @@ const App = () => {
           public_token: publicToken,
         }
       );
-      console.log('res.data', res.data);
+      setCredentials(res.data);
+      console.log('exchangeCredentials res.data', res.data);
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
+
+  const [connection, setConnection] = React.useState<any>();
+  const fetchConnection = async () => {
+    try {
+      const res = await axios.get(
+        'https://sandbox.rutterapi.com/connections/access_token',
+        {
+          params: {
+            access_token: credentials!.access_token,
+          },
+          auth: {
+            username: process.env.REACT_APP_CLIENT_ID || '',
+            password: process.env.REACT_APP_SECRET_KEY || '',
+          },
+        }
+      );
+
+      console.log('fetchConnection res.data', res);
+      setConnection(res.data);
     } catch (e: any) {
       console.log(e);
     }
@@ -36,7 +70,13 @@ const App = () => {
         height: '100vh',
       }}
     >
-      <button onClick={() => open()}>Open</button>
+      {connection ? (
+        <pre>{connection}</pre>
+      ) : credentials ? (
+        <button onClick={() => fetchConnection()}>Fetch Connection</button>
+      ) : (
+        <button onClick={() => open()}>Open Rutter Link</button>
+      )}
     </div>
   );
 };
